a36bc8f29a427c32c3ae4bb0addc08097cebfbca
Skylot <skylot@gmail.com>
2014-06-24 14:07:53 +0400

    core: add serial uid to JadxRuntimeException

--- jadx-core/src/main/java/jadx/core/utils/exceptions/JadxOverflowException.java
+++ jadx-core/src/main/java/jadx/core/utils/exceptions/JadxOverflowException.java
@@ -1,6 +1,9 @@
 package jadx.core.utils.exceptions;
 
 public class JadxOverflowException extends JadxRuntimeException {
+
+	private static final long serialVersionUID = 2568659798680154204L;
+
 	public JadxOverflowException(String message) {
 		super(message);
 	}
