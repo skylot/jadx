d32a6846c5543b79f107bc4b1813c583105aae4f
a.sviridenko <a.sviridenko@s-e.su>
2018-05-25 17:52:14 +0300

    add certificate panel

---
+++ jadx-gui/src/main/java/jadx/gui/ui/CertificatePanel.java
@@ -0,0 +1,18 @@
+package jadx.gui.ui;
+
+import jadx.gui.treemodel.JNode;
+
+import java.awt.*;
+
+public class CertificatePanel extends ContentPanel {
+    CertificatePanel(TabbedPane panel, JNode jnode) {
+        super(panel, jnode);
+        setLayout(new BorderLayout());
+       // add(new )
+    }
+
+    @Override
+    public void loadSettings() {
+
+    }
+}
