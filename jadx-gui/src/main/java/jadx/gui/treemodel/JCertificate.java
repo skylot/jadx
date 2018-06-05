6ee0ade4b7bb33dbd4dd4003ba32f9304efc35a4
a.sviridenko <a.sviridenko@s-e.su>
2018-06-05 18:50:17 +0300

    ресурсы

--- jadx-gui/src/main/java/jadx/gui/treemodel/JCertificate.java
+++ jadx-gui/src/main/java/jadx/gui/treemodel/JCertificate.java
@@ -1,17 +1,24 @@
 package jadx.gui.treemodel;
 import jadx.api.ResourceFile;
 
+import jadx.core.utils.exceptions.JadxException;
+import jadx.core.utils.files.ZipSecurity;
+import jadx.gui.utils.CertificateManager;
 import jadx.gui.utils.NLS;
 import jadx.gui.utils.Utils;
 import javax.swing.*;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class JCertificate extends JNode {
 
 	private static final ImageIcon CERTIFICATE_ICON = Utils.openIcon("certificate_obj");
-    private final transient ResourceFile resFile;
+    private final transient ResourceFile rf;
 
 	public JCertificate(ResourceFile resFile) {
-        this.resFile = resFile;
+        this.rf = resFile;
 	}
 
 	@Override
@@ -31,6 +38,41 @@ public class JCertificate extends JNode {
 
 	@Override
 	public String getContent() {
+
+/*
+		ResourceFile.ZipRef zipRef = rf.getZipRef();
+		if (zipRef == null) {
+			File file = new File(rf.getName());
+			LinkedList<File> list = new LinkedList<>();
+			list.add(file);
+			return CertificateManager.findCertificates(list);
+		}
+*/
+		try {
+			ResourceFile.ZipRef zipRef = rf.getZipRef();
+			if (zipRef == null) {
+				File file = new File(rf.getName());
+				try (InputStream inputStream = new BufferedInputStream(new FileInputStream(file))) {
+					return CertificateManager.decode(inputStream);
+				}
+			} else {
+				try (ZipFile zipFile = new ZipFile(zipRef.getZipFile())) {
+					ZipEntry entry = zipFile.getEntry(zipRef.getEntryName());
+					if (entry == null) {
+						throw new IOException("Zip entry not found: " + zipRef);
+					}
+					if (!ZipSecurity.isValidZipEntry(entry)) {
+						return null;
+					}
+					try (InputStream inputStream = new BufferedInputStream(zipFile.getInputStream(entry))) {
+						return  CertificateManager.decode(inputStream);
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		//	throw new JadxException("Error decode: " + rf.getName(), e);
+		}
 		/*
 		if (!loaded && resFile != null && type == JResource.JResType.FILE) {
 			loaded = true;
--- jadx-gui/src/main/java/jadx/gui/utils/CertificateManager.java
+++ jadx-gui/src/main/java/jadx/gui/utils/CertificateManager.java
@@ -17,6 +17,7 @@ import java.util.LinkedList;
 
 public class CertificateManager {
 
+	/*
 	private String info;
 
 	CertificateManager(LinkedList<File> allFiles) {
@@ -27,7 +28,7 @@ public class CertificateManager {
 		return info;
 	}
 
-	static String findCertificates(final LinkedList<File> allFiles) {
+	public static String findCertificates(final LinkedList<File> allFiles) {
 		ArrayList<Certificate> certs = new ArrayList<>(2);
 		for (File file : allFiles) {
 			String path = file.getAbsolutePath();
@@ -55,73 +56,96 @@ public class CertificateManager {
 		return null;
 	}
 
-	static String generateText(ArrayList<Certificate> certificates) {
+*/
 
-		StringBuffer str = new StringBuffer();
-		for (Certificate cert : certificates) {
 
-			String type = cert.getType();
-			append(str,NLS.str("CertType"), type);
-			if (!type.equals("X.509")) {
-				str.append(cert.toString());
+	static public String decode(InputStream in){
+		StringBuilder strBuild = new StringBuilder();
+		Collection<? extends Certificate> certificates =readCertificates(in);
+		for(Certificate cert:certificates){
+			strBuild.append(generateText(cert));
+		}
+		return strBuild.toString();
+	}
+	static private Collection<? extends Certificate> readCertificates(InputStream in) {
+		CertificateFactory cf;
+		try {
+			cf = CertificateFactory.getInstance("X.509");
+			Collection<? extends Certificate> certs = cf.generateCertificates(in);
+			in.close();
+			return certs;
+		} catch (Exception e) {
 
-			} else {
-				// ��� ���� "X.509" ������� ������
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-				if (cert instanceof X509Certificate) {
-					X509Certificate x509cert = (X509Certificate) cert;
 
-					// Get subject
-					Principal principal = x509cert.getSubjectDN();
-					append(str, NLS.str("CertSubject"), principal.getName());
+	static String generateText(Certificate cert) {
+		StringBuilder str = new StringBuilder();
+		String type = cert.getType();
+		append(str, NLS.str("certificate.certType"), type);
+		if (!type.equals("X.509")) {
+			str.append(cert.toString());
 
-					// Get issuer
-					principal = x509cert.getIssuerDN();
-					append(str, NLS.str("CertIssuer"), principal.getName());
+		} else {
 
-					// seral number
-					append(str, NLS.str("SerialNumber"), "0x"
-							+ x509cert.getSerialNumber().toString(16));
 
-					append(str, NLS.str("SerialValidFrom"), x509cert.getNotBefore()
-							.toString());
-					append(str, NLS.str("SerialValidUntil"), x509cert.getNotAfter()
-							.toString());
-					str.append("\n");
+			if (cert instanceof X509Certificate) {
+				X509Certificate x509cert = (X509Certificate) cert;
 
-					append(str, NLS.str("SerialSignature"), "");
-					append(str, NLS.str("SerialSigVer"),
-							((Integer) x509cert.getVersion()).toString());
-					append(str, NLS.str("SerialAlgName"), x509cert.getSigAlgName());
-					append(str, NLS.str("SerialSigOID"), x509cert.getSigAlgOID());
-					// Fingerprint:
-					try {
-						append(str, NLS.str("SerialMD5"),getThumbPrint(x509cert, "MD5"));
-						append(str, NLS.str("SerialSHA1"),getThumbPrint(x509cert, "SHA-1"));
-						append(str, NLS.str("SerialSHA256"),getThumbPrint(x509cert, "SHA-256"));
-					} catch (CertificateEncodingException
-							| NoSuchAlgorithmException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					str.append("\n");
+				// Get subject
+				Principal principal = x509cert.getSubjectDN();
+				append(str, NLS.str("certificate.сertSubject"), principal.getName());
 
-					RSAPublicKey pub = (RSAPublicKey) cert.getPublicKey();
+				// Get issuer
+				principal = x509cert.getIssuerDN();
+				append(str, NLS.str("certificate.сertIssuer"), principal.getName());
 
-					append(str, NLS.str("SerialPubKey"), "");
-					append(str, NLS.str("SerialAlgName"), pub.getAlgorithm());
-					append(str, NLS.str("SerialPubKeyExponent"), pub.getPublicExponent().toString(16));
-					append(str, NLS.str("SerialPubKeyModulus"), pub.getModulus().toString(10));
+				// seral number
+				append(str, NLS.str("certificate.serialNumber"), "0x"
+						+ x509cert.getSerialNumber().toString(16));
 
+				append(str, NLS.str("certificate.serialValidFrom"), x509cert.getNotBefore()
+						.toString());
+				append(str, NLS.str("certificate.serialValidUntil"), x509cert.getNotAfter()
+						.toString());
+				str.append("\n");
+
+				append(str, NLS.str("certificate.serialSignature"), "");
+				append(str, NLS.str("certificate.serialSigVer"),
+						((Integer) x509cert.getVersion()).toString());
+				append(str, NLS.str("certificate.serialAlgName"), x509cert.getSigAlgName());
+				append(str, NLS.str("certificate.serialSigOID"), x509cert.getSigAlgOID());
+				// Fingerprint:
+				try {
+					append(str, NLS.str("certificate.sSerialMD5"), getThumbPrint(x509cert, "MD5"));
+					append(str, NLS.str("certificate.serialSHA1"), getThumbPrint(x509cert, "SHA-1"));
+					append(str, NLS.str("certificate.serialSHA256"), getThumbPrint(x509cert, "SHA-256"));
+				} catch (CertificateEncodingException
+						| NoSuchAlgorithmException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
+				str.append("\n");
+
+				RSAPublicKey pub = (RSAPublicKey) cert.getPublicKey();
+
+				append(str, NLS.str("certificate.serialPubKey"), "");
+				append(str, NLS.str("certificate.serialAlgName"), pub.getAlgorithm());
+				append(str, NLS.str("certificate.serialPubKeyExponent"), pub.getPublicExponent().toString(16));
+				append(str, NLS.str("certificate.serialPubKeyModulus"), pub.getModulus().toString(10));
 
 			}
-		}
 
+		}
 		return str.toString();
 	}
 
-	static void append(StringBuffer str, String name, String value) {
+
+
+	static void append(StringBuilder str, String name, String value) {
 		str.append(name + ": " + value + "\n");
 	}
 
@@ -137,8 +161,8 @@ public class CertificateManager {
 
 	public static String hexify(byte bytes[]) {
 
-		char[] hexDigits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-				'A', 'B', 'C', 'D', 'E', 'F' };
+		char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+				'A', 'B', 'C', 'D', 'E', 'F'};
 
 		StringBuffer buf = new StringBuffer(bytes.length * 3);
 
