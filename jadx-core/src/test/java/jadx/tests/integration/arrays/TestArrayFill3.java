6fca311de01c6b53fc8e13df704b7d9fad286584
Ahmed Ashour <asashour@yahoo.com>
2019-03-29 14:10:33 +0100

    test: add test case for #536 (PR #537)

---
+++ jadx-core/src/test/java/jadx/tests/integration/arrays/TestArrayFill3.java
@@ -0,0 +1,28 @@
+package jadx.tests.integration.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import jadx.core.dex.nodes.ClassNode;
+import jadx.tests.api.IntegrationTest;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestArrayFill3 extends IntegrationTest {
+
+	public static class TestCls {
+
+		public byte[] test(int a) {
+			return new byte[]{0, 1, 2};
+		}
+	}
+
+	@Test
+	public void test() {
+		ClassNode cls = getClassNode(TestCls.class);
+		String code = cls.getCode().toString();
+
+		assertThat(code, containsString("return new byte[]"));
+	}
+
+}
