d80d187537c56d8ee2ed55e1f537851e386647c2
Ahmed Ashour <asashour@yahoo.com>
2019-04-08 12:34:24 +0200

    fix: remove field redundant array type when initialized

---
+++ jadx-core/src/test/java/jadx/tests/integration/arrays/TestArrayInitField.java
@@ -0,0 +1,24 @@
+package jadx.tests.integration.arrays;
+
+import jadx.core.dex.nodes.ClassNode;
+import jadx.tests.api.IntegrationTest;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestArrayInitField extends IntegrationTest {
+
+	public static class TestCls {
+
+		byte[] bytes = new byte[]{10, 20, 30};
+	}
+
+	@Test
+	public void test() {
+		ClassNode cls = getClassNode(TestCls.class);
+		String code = cls.getCode().toString();
+
+		assertThat(code, containsString("= {10, 20, 30};"));
+	}
+}
