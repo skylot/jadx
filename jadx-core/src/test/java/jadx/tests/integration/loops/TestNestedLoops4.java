192b543f309c7054ef5a92e032a219232d85692c
Ahmed Ashour <asashour@yahoo.com>
2019-04-09 10:20:05 +0200

    test: add test case for #130

---
+++ jadx-core/src/test/java/jadx/tests/integration/loops/TestNestedLoops4.java
@@ -0,0 +1,38 @@
+package jadx.tests.integration.loops;
+
+import org.junit.jupiter.api.Test;
+
+import jadx.NotYetImplemented;
+import jadx.tests.api.IntegrationTest;
+
+public class TestNestedLoops4 extends IntegrationTest {
+
+	public static class TestCls {
+
+		public int testFor() {
+            int tmp = 1;
+            for (int i = 10; i > -1; i--) {
+                if (i > tmp) {
+                    for (int j = 0; j < 54; j += 4) {
+                        if (i < j) {
+                            for (int k = j; k < j + 4; k++) {
+                                if (tmp> k) {
+                                    return 0;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+                tmp++;
+            }
+            return tmp;
+        }
+	}
+
+	@Test
+	@NotYetImplemented
+	public void test() {
+		getClassNode(TestCls.class);
+	}
+}
