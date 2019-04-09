a47d8ae74163c1732593c0fc962e862c8cdd6c44
Ahmed Ashour <asashour@yahoo.com>
2019-04-09 10:12:14 +0200

    test: add test case for #101

---
+++ jadx-core/src/test/java/jadx/tests/integration/trycatch/TestTryWithResources.java
@@ -0,0 +1,39 @@
+package jadx.tests.integration.trycatch;
+
+import static jadx.tests.api.utils.JadxMatchers.containsOne;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.junit.jupiter.api.Test;
+
+import jadx.NotYetImplemented;
+import jadx.core.dex.nodes.ClassNode;
+import jadx.tests.api.SmaliTest;
+
+public class TestTryWithResources extends SmaliTest {
+
+	public static class TestCls {
+
+		public static void writeFully(File file, byte[] data) throws IOException {
+			try (OutputStream out = new FileOutputStream(file)) {
+				out.write(data);
+			}
+		}
+	}
+
+	@Test
+	@NotYetImplemented
+	public void test() {
+		ClassNode cls = getClassNode(TestCls.class);
+		String code = cls.getCode().toString();
+
+		assertThat(code, containsOne("try ("));
+		assertThat(code, not(containsString("close()")));
+	}
+}
