827a048838d8a961b3c1f0dc3e9d3854a28fcd0e
Ahmed Ashour <asashour@yahoo.com>
2019-04-09 09:19:51 +0200

    test: add two cases for switch-try-break combination

---
+++ jadx-core/src/test/java/jadx/tests/integration/conditions/TestSwitchTryBreak2.java
@@ -0,0 +1,41 @@
+package jadx.tests.integration.conditions;
+
+import org.junit.jupiter.api.Test;
+
+import jadx.NotYetImplemented;
+import jadx.tests.api.IntegrationTest;
+
+public class TestSwitchTryBreak2 extends IntegrationTest {
+
+	public static class TestCls {
+
+		public void test(int x) {
+			switch(x) {
+				case 0:
+					return;
+				case 1:
+					String res;
+					if ("android".equals(toString())) {
+						res = "hello";
+					} else {
+						try {
+							if (x == 5) {
+								break;
+							}
+							res = "hi";
+						} catch (Exception e) {
+							break;
+						}
+					}
+					System.out.println(res);
+			}
+			System.out.println("returning");
+		}
+	}
+
+	@Test
+	@NotYetImplemented
+	public void test() {
+		getClassNode(TestCls.class);
+	}
+}
