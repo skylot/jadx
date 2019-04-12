eb77aa51b2b6693f2bb10c3a6dfd63873db71027
Ahmed Ashour <asashour@yahoo.com>
2019-04-12 17:11:22 +0200

    fix: conditions in ternary if (#449) (PR #558)

---
+++ jadx-core/src/test/java/jadx/tests/integration/conditions/TestTernaryInIf2.java
@@ -0,0 +1,42 @@
+package jadx.tests.integration.conditions;
+
+import static jadx.tests.api.utils.JadxMatchers.containsLines;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import jadx.core.dex.nodes.ClassNode;
+import jadx.tests.api.SmaliTest;
+
+public class TestTernaryInIf2 extends SmaliTest {
+
+	public static class TestCls {
+	    private String a;
+	    private String b;
+
+	    public boolean equals(TestCls other) {
+	    	if (this.a == null ? other.a == null : this.a.equals(other.a)) {
+    			if (this.b == null ? other.b == null : this.b.equals(other.b)) {
+    				return true;
+    			}
+	    	}
+	    	return false;
+	    }
+	}
+
+	@Test
+	public void test() {
+		ClassNode cls = getClassNode(TestCls.class);
+		String code = cls.getCode().toString();
+
+		assertThat(code, containsLines(2, "if (this.a != null ? this.a.equals(other.a) : other.a == null) {"));
+		assertThat(code, containsLines(3, "if (this.b != null ? this.b.equals(other.b) : other.b == null) {"));
+		assertThat(code, containsLines(4, "return true;"));
+		assertThat(code, containsLines(2, "return false;"));
+	}
+
+	@Test
+	public void test2() {
+		getClassNodeFromSmaliWithPath("conditions", "TestTernaryInIf2");
+	}
+}
