2148d4b0f589d76a802c3c11b5f326e3825e3742
Ahmed Ashour <asashour@yahoo.com>
2019-04-15 20:25:47 +0200

    test: add test case for #597 (PR #603)

---
+++ jadx-core/src/test/java/jadx/tests/integration/inner/TestAnonymousClass16.java
@@ -0,0 +1,36 @@
+package jadx.tests.integration.inner;
+
+import org.junit.jupiter.api.Test;
+
+import jadx.NotYetImplemented;
+import jadx.tests.api.IntegrationTest;
+
+public class TestAnonymousClass16 extends IntegrationTest {
+
+	public static class TestCls {
+
+		public Something test() {
+			Something a = new Something() {
+				{
+					put("a", "b");
+				}
+			};
+			a.put("c", "d");
+			return a;
+		}
+
+		public class Something {
+			public void put(Object o, Object o2) {
+			}
+		}
+	}
+
+
+	@Test
+	@NotYetImplemented
+	public void test() {
+		noDebugInfo();
+		getClassNode(TestCls.class);
+	}
+}
+
