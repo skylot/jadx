3b09c1014ca3f5e34f76ebef5f9fbd83010277d3
Ahmed Ashour <asashour@yahoo.com>
2019-04-24 13:11:29 +0200

    test: add test case for #628

---
+++ jadx-core/src/test/java/jadx/tests/integration/conditions/TestConditions17.java
@@ -0,0 +1,39 @@
+package jadx.tests.integration.conditions;
+
+import org.junit.jupiter.api.Test;
+
+import jadx.NotYetImplemented;
+import jadx.core.dex.nodes.ClassNode;
+import jadx.tests.api.IntegrationTest;
+
+import static jadx.tests.api.utils.JadxMatchers.containsOne;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestConditions17 extends IntegrationTest {
+
+	public static class TestCls {
+
+		public static final int SOMETHING = 2;
+
+		public static void test(int a) {
+			if ((a & SOMETHING) != 0) {
+				print(1);
+			}
+			print(2);
+		}
+
+		public static void print(Object o) {
+		}
+	}
+
+	@Test
+	@NotYetImplemented
+	public void test() {
+		ClassNode cls = getClassNode(TestCls.class);
+		String code = cls.getCode().toString();
+
+		assertThat(code, containsOne(" & "));
+	}
+}
