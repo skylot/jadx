c7dadd28dfbd4cddfdad119a8f487798c1d3483a
S-trace <S-trace@list.ru>
2019-12-25 20:24:58 +0300

    core: ClassNode: Load recursively missing information on refresh()
    Fixes loss of static identifiers, comments, annotations and source file name after rename.

--- jadx-core/src/main/java/jadx/core/dex/attributes/AType.java
+++ jadx-core/src/main/java/jadx/core/dex/attributes/AType.java
@@ -86,5 +86,9 @@ public class AType<T extends IAttribute> {
 	public static final Set<AType<?>> SKIP_ON_UNLOAD = new HashSet<>(Arrays.asList(
 			FIELD_REPLACE,
 			METHOD_INLINE,
+			COMMENTS,
+			RENAME_REASON,
+			JADX_WARN,
+			JADX_ERROR,
 			SKIP_MTH_ARGS));
 }
