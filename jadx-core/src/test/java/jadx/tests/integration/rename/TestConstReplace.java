c777875b44519a4475ccedaa873e1c1c4873056c
Skylot <skylot@gmail.com>
2020-01-05 17:28:25 +0300

    fix: deep reload for inner classes, const values and anonymous classes

---
+++ jadx-core/src/test/java/jadx/tests/integration/rename/TestConstReplace.java
@@ -0,0 +1,30 @@
+package jadx.tests.integration.rename;
+
+import org.junit.jupiter.api.Test;
+
+import jadx.core.dex.nodes.ClassNode;
+import jadx.tests.api.IntegrationTest;
+
+import static jadx.tests.api.utils.assertj.JadxAssertions.assertThat;
+
+public class TestConstReplace extends IntegrationTest {
+
+	public static class TestCls {
+		public static final String CONST = "SOME_CONST";
+
+		public String test() {
+			return CONST;
+		}
+	}
+
+	@Test
+	public void test() {
+		ClassNode cls = getClassNode(TestCls.class);
+		assertThat(cls.getCode())
+				.containsOnlyOnce("return CONST;");
+
+		assertThat(cls.reloadCode())
+				.print()
+				.containsOnlyOnce("return CONST;");
+	}
+}
