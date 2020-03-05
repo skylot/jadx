b0484cdb7288c027cbcbeb149b9a7b3913771d33
acsbendi <acsbendi28@gmail.com>
2020-03-05 23:03:35 +0100

    Added failing test for super member shadowing.

---
+++ jadx-core/src/test/java/jadx/tests/integration/others/TestShadowingSuperMember.java
@@ -0,0 +1,46 @@
+package jadx.tests.integration.others;
+
+import jadx.core.dex.nodes.ClassNode;
+import jadx.tests.api.SmaliTest;
+import org.junit.jupiter.api.Test;
+
+import static jadx.tests.api.utils.JadxMatchers.containsOne;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestShadowingSuperMember extends SmaliTest {
+	// @formatter:off
+	/*
+
+		public class C {
+			public C(String s) {
+			}
+		}
+
+		public class A {
+			public int a;
+			public A(String s) {
+			}
+		}
+
+		public class B extends A {
+			public C a;
+			public B(String str) {
+				super(str);
+			}
+
+			public int add(int b) {
+				return super.a + b;
+			}
+		}
+	*/
+	// @formatter:on
+
+	@Test
+	public void test() {
+		allowWarnInCode();
+		ClassNode cls = getClassNodeFromSmaliFiles("B");
+		String code = cls.getCode().toString();
+
+		assertThat(code, containsOne("return super.a + b;"));
+	}
+}
---
+++ jadx-core/src/test/smali/others/TestShadowingSuperMember/C.smali
@@ -0,0 +1,12 @@
+.class public Lothers/C;
+.super Ljava/lang/Object;
+
+.method public constructor <init>(Ljava/lang/String;)V
+    .registers 3
+
+    .prologue
+
+    invoke-direct {p0}, Ljava/lang/Object;-><init>()V
+
+    return-void
+.end method
