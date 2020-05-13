aa80ced88ffcda404bd6882b6f1d1d58e482974e
Soul Trace <S-trace@list.ru>
2020-05-14 00:00:03 +0300

    gui: RenameDialog: Warn user if deobfuscation settings are invalid
    The rename functionality relies on deobfuscation for now - so let the user know
    this and ask the user to enable deobfuscation to get rename working.
    
    The "Force rewrite deobfuscation map file" option effectively disables
    renaming, because renaming relies on deobfuscation map modification for now,
    but Force rewrite rewrites the map on each file reload, destroying changes.
    So. let the user know this issue instead of silent failure.
    
    TODO: Split rename map and deobfuscation map (for example, load the rename map
    file after deobfuscation map file to override autogenerated aliases).

--- jadx-gui/src/main/java/jadx/gui/ui/RenameDialog.java
+++ jadx-gui/src/main/java/jadx/gui/ui/RenameDialog.java
@@ -31,6 +31,7 @@ import jadx.core.utils.files.InputFile;
 import jadx.gui.jobs.IndexJob;
 import jadx.gui.jobs.RefreshJob;
 import jadx.gui.jobs.UnloadJob;
+import jadx.gui.settings.JadxSettings;
 import jadx.gui.treemodel.JClass;
 import jadx.gui.treemodel.JField;
 import jadx.gui.treemodel.JMethod;
@@ -39,10 +40,7 @@ import jadx.gui.treemodel.JPackage;
 import jadx.gui.ui.codearea.ClassCodeContentPanel;
 import jadx.gui.ui.codearea.CodeArea;
 import jadx.gui.ui.codearea.CodePanel;
-import jadx.gui.utils.CacheObject;
-import jadx.gui.utils.CodeUsageInfo;
-import jadx.gui.utils.NLS;
-import jadx.gui.utils.TextStandardActions;
+import jadx.gui.utils.*;
 
 public class RenameDialog extends JDialog {
 	private static final long serialVersionUID = -3269715644416902410L;
@@ -62,8 +60,39 @@ public class RenameDialog extends JDialog {
 		mainWindow = codeArea.getMainWindow();
 		this.codeArea = codeArea;
 		this.node = node;
-		initUI();
-		loadWindowPos();
+		if (isDeobfuscationSettingsValid()) {
+			initUI();
+			loadWindowPos();
+		} else {
+			LOG.error("Deobfuscation settings are invalid - please enable deobfuscation and disable force rewrite deobfuscation map");
+		}
+	}
+
+	private boolean isDeobfuscationSettingsValid() {
+		boolean valid = true;
+		String errorMessage = null;
+		JadxSettings settings = mainWindow.getSettings();
+		final LangLocale langLocale = settings.getLangLocale();
+		if (settings.isDeobfuscationForceSave()) {
+			valid = false;
+			errorMessage = NLS.str("msg.rename_disabled_force_rewrite_enabled", langLocale);
+		}
+		if (!settings.isDeobfuscationOn()) {
+			valid = false;
+			errorMessage = NLS.str("msg.rename_disabled_deobfuscation_disabled", langLocale);
+		}
+		if (errorMessage != null) {
+			showRenameDisabledErrorMessage(langLocale, errorMessage);
+		}
+		return valid;
+	}
+
+	private void showRenameDisabledErrorMessage(LangLocale langLocale, String message) {
+		JOptionPane.showMessageDialog(
+				mainWindow,
+				message,
+				NLS.str("msg.rename_disabled_title", langLocale),
+				JOptionPane.ERROR_MESSAGE);
 	}
 
 	private void loadWindowPos() {
