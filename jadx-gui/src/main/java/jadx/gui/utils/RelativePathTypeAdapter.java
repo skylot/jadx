1c4c626edccfaeb0ed4f35e3573efe49a0f04641
Jan Peter Stotz <jpstotz@users.noreply.github.com>
2021-12-20 12:10:22 +0100

    chore: use relative file paths in .jadx project file (#1312)

---
+++ jadx-gui/src/main/java/jadx/gui/utils/RelativePathTypeAdapter.java
@@ -0,0 +1,43 @@
+package jadx.gui.utils;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+public class RelativePathTypeAdapter extends TypeAdapter<Path> {
+	private final Path basePath;
+
+	public RelativePathTypeAdapter(Path basePath) {
+		this.basePath = basePath;
+	}
+
+	@Override
+	public void write(JsonWriter out, Path value) throws IOException {
+		if (value == null) {
+			out.nullValue();
+		} else {
+			value = value.toAbsolutePath().normalize();
+			String relativePath = basePath.relativize(value).toString();
+			out.value(relativePath);
+		}
+	}
+
+	@Override
+	public Path read(JsonReader in) throws IOException {
+		if (in.peek() == JsonToken.NULL) {
+			in.nextNull();
+			return null;
+		}
+		Path p = Paths.get(in.nextString());
+		if (p.isAbsolute()) {
+			return p;
+		}
+		return basePath.resolve(p);
+	}
+
+}
