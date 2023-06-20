afa331e8fc5c826d13f70d602c88c71b41056cda
chaos <71205599+chaos-cn@users.noreply.github.com>
2023-06-21 01:36:37 +0800

    feat(gui): register macOS specific open file handler  (#1922)(PR #1923)
    * feature:支持双击文件后直接打开jadx-gui
    
    * additional checks
    
    ---------
    
    Co-authored-by: Skylot <skylot@gmail.com>

--- jadx-gui/src/main/java/jadx/gui/JadxGUI.java
+++ jadx-gui/src/main/java/jadx/gui/JadxGUI.java
@@ -1,11 +1,14 @@
 package jadx.gui;
 
+import java.awt.Desktop;
+
 import javax.swing.SwingUtilities;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import jadx.cli.LogHelper;
+import jadx.core.utils.files.FileUtils;
 import jadx.gui.logs.LogCollector;
 import jadx.gui.settings.JadxSettings;
 import jadx.gui.settings.JadxSettingsAdapter;
@@ -37,10 +40,24 @@ public class JadxGUI {
 			SwingUtilities.invokeLater(() -> {
 				MainWindow mw = new MainWindow(settings);
 				mw.init();
+				registerOpenFileHandler(mw);
 			});
 		} catch (Exception e) {
 			LOG.error("Error: {}", e.getMessage(), e);
 			System.exit(1);
+		}
+	}
+
+	private static void registerOpenFileHandler(MainWindow mw) {
+		try {
+			if (Desktop.isDesktopSupported()) {
+				Desktop desktop = Desktop.getDesktop();
+				if (desktop.isSupported(Desktop.Action.APP_OPEN_FILE)) {
+					desktop.setOpenFileHandler(e -> mw.open(FileUtils.toPaths(e.getFiles())));
+				}
+			}
+		} catch (Throwable e) {
+			LOG.error("Failed to register open file handler", e);
 		}
 	}
 
