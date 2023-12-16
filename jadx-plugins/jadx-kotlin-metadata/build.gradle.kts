68a99c541085a257121707bc8d2ca6e0032b8a73
Skylot <skylot@gmail.com>
2023-12-16 16:05:51 +0000

    chore: update dependencies

--- jadx-plugins/jadx-kotlin-metadata/build.gradle.kts
+++ jadx-plugins/jadx-kotlin-metadata/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 dependencies {
 	api(project(":jadx-core"))
 
-	implementation("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.7.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-metadata-jvm:0.8.0")
 
 	testImplementation(project(":jadx-core").dependencyProject.sourceSets.test.get().output)
 	testImplementation("org.apache.commons:commons-lang3:3.14.0")
