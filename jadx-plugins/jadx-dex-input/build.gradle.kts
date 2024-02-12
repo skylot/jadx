21b14524855ea3e338881d2001f1b92349463266
Skylot <skylot@gmail.com>
2024-02-12 15:39:30 +0000

    chore: update gradle and dependencies

--- jadx-plugins/jadx-dex-input/build.gradle.kts
+++ jadx-plugins/jadx-dex-input/build.gradle.kts
@@ -6,13 +6,13 @@ dependencies {
 	api(project(":jadx-core"))
 
 	// TODO: finish own smali printer
-	implementation("com.android.tools.smali:smali-baksmali:3.0.3") {
+	implementation("com.android.tools.smali:smali-baksmali:3.0.4") {
 		exclude(group = "com.beust", module = "jcommander") // exclude old jcommander namespace
 	}
 	implementation("com.google.guava:guava:33.0.0-jre") // force the latest version for smali
 
 	// compile smali files in tests
-	testImplementation("com.android.tools.smali:smali:3.0.3") {
+	testImplementation("com.android.tools.smali:smali:3.0.4") {
 		exclude(group = "com.beust", module = "jcommander") // exclude old jcommander namespace
 	}
 }
