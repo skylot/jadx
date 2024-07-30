ae6ca2b7b5039b37cf3436fa325d41ce506198a3
Mino260806 <mino260806@gmail.com>
2024-07-30 23:17:59 +0100

    feat: export resource folder

--- jadx-gui/src/main/java/jadx/gui/ui/popupmenu/JResourcePopupMenu.java
+++ jadx-gui/src/main/java/jadx/gui/ui/popupmenu/JResourcePopupMenu.java
@@ -2,10 +2,12 @@ package jadx.gui.ui.popupmenu;
 
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
@@ -13,6 +15,9 @@ import java.util.Locale;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 
+import jadx.core.utils.files.FileUtils;
+import jadx.gui.treemodel.JClass;
+import jadx.gui.treemodel.JPackage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,61 +39,126 @@ public class JResourcePopupMenu extends JPopupMenu {
 	public JResourcePopupMenu(MainWindow mainWindow, JResource resource) {
 		this.mainWindow = mainWindow;
 
-		add(makeExportMenuItem(resource));
+		if (resource.getType() != JResource.JResType.ROOT) {
+			add(makeExportMenuItem(resource));
+		}
 	}
 
 	private JMenuItem makeExportMenuItem(JResource resource) {
 		JMenuItem exportMenu = new JMenuItem(NLS.str("popup.export"));
 		exportMenu.addActionListener(event -> {
-			String extension = CommonFileUtils.getFileExtension(resource.getName());
-			System.out.println(resource.getResFile().getType());
-
-			FileDialogWrapper fileDialog = new FileDialogWrapper(mainWindow, FileOpenMode.EXPORT_NODE);
-			fileDialog.setFileExtList(Collections.singletonList(extension));
-			Path currentDir = fileDialog.getCurrentDir();
-			if (currentDir != null) {
-				fileDialog.setSelectedFile(currentDir.resolve(resource.getName()));
+			Path savePath = null;
+			switch (resource.getType()) {
+				case ROOT:
+				case DIR:
+					savePath = getSaveDirPath(resource);
+					break;
+				case FILE:
+					savePath = getSaveFilePath(resource);
+					break;
 			}
 
-			List<Path> selectedPaths = fileDialog.show();
-			if (selectedPaths.size() != 1) {
+			if (savePath == null) {
 				return;
 			}
 
-			Path selectedPath = selectedPaths.get(0);
-			Path savePath;
-			// Append file extension if missing
-			if (extension != null &&
-					!selectedPath.getFileName().toString().toLowerCase(Locale.ROOT).endsWith(extension)) {
-				savePath = selectedPath.resolveSibling(selectedPath.getFileName() + "." + extension);
-			} else {
-				savePath = selectedPath;
-			}
-
-			switch (resource.getResFile().getType()) {
-				case MANIFEST:
-				case XML:
-					exportString(savePath, resource);
-					break;
-				default:
-					exportBinary(savePath, resource);
-					break;
-			}
+			saveJResource(resource, savePath, true);
 
 			LOG.info("Done saving " + savePath);
 		});
 		return exportMenu;
 	}
 
-	private static void exportString(Path savePath, JResource resource) {
-		try (Writer writer = Files.newBufferedWriter(savePath, StandardCharsets.UTF_8)) {
-			writer.write(resource.getCodeInfo().getCodeStr());
-		} catch (Exception e) {
-			throw new RuntimeException("Error saving project", e);
+	private Path getSaveFilePath(JResource resource) {
+		String extension = CommonFileUtils.getFileExtension(resource.getName());
+
+		FileDialogWrapper fileDialog = new FileDialogWrapper(mainWindow, FileOpenMode.EXPORT_NODE);
+		fileDialog.setFileExtList(Collections.singletonList(extension));
+		Path currentDir = fileDialog.getCurrentDir();
+		if (currentDir != null) {
+			fileDialog.setSelectedFile(currentDir.resolve(resource.getName()));
+		}
+
+		List<Path> selectedPaths = fileDialog.show();
+		if (selectedPaths.size() != 1) {
+			return null;
+		}
+
+		Path selectedPath = selectedPaths.get(0);
+		Path savePath;
+		// Append file extension if missing
+		if (extension != null &&
+				!selectedPath.getFileName().toString().toLowerCase(Locale.ROOT).endsWith(extension)) {
+			savePath = selectedPath.resolveSibling(selectedPath.getFileName() + "." + extension);
+		} else {
+			savePath = selectedPath;
+		}
+
+
+		return savePath;
+	}
+	private Path getSaveDirPath(JResource resource) {
+		FileDialogWrapper fileDialog = new FileDialogWrapper(mainWindow, FileOpenMode.EXPORT_NODE_FOLDER);
+
+		List<Path> selectedPaths = fileDialog.show();
+		if (selectedPaths.size() != 1) {
+			return null;
+		}
+
+        return selectedPaths.get(0);
+	}
+
+	private static void saveJResource(JResource resource, Path savePath, boolean comingFromDialog) {
+		switch (resource.getType()) {
+			case ROOT:
+			case DIR:
+				saveJResourceDir(resource, savePath, comingFromDialog);
+				break;
+			case FILE:
+				saveJResourceFile(resource, savePath, comingFromDialog);
+				break;
 		}
 	}
 
-	private static void exportBinary(Path savePath, JResource resource) {
+	private static void saveJResourceDir(JResource resource, Path savePath, boolean comingFromDialog) {
+		Path subSavePath = savePath.resolve(resource.getName());
+		try {
+			if (!Files.isDirectory(subSavePath)) {
+				Files.createDirectory(subSavePath);
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		for (JResource subResource: resource.getSubNodes()) {
+			saveJResource(subResource, subSavePath, false);
+		}
+	}
+
+	private static void saveJResourceFile(JResource resource, Path savePath, boolean comingFromDialog) {
+		if (!comingFromDialog) {
+			Path fileName = Path.of(resource.getName()).getFileName();
+			savePath = savePath.resolve(fileName);
+		}
+		switch (resource.getResFile().getType()) {
+			case MANIFEST:
+			case XML:
+				exportString(resource, savePath);
+				break;
+			default:
+				exportBinary(resource, savePath);
+				break;
+		}
+	}
+
+	private static void exportString(JResource resource, Path savePath) {
+		try (Writer writer = Files.newBufferedWriter(savePath, StandardCharsets.UTF_8)) {
+			writer.write(resource.getCodeInfo().getCodeStr());
+		} catch (Exception e) {
+			throw new RuntimeException("Error saving file " + resource.getName(), e);
+		}
+	}
+
+	private static void exportBinary(JResource resource, Path savePath) {
 		try (BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream(savePath.toFile()))) {
 			byte[] bytes = ResourcesLoader.decodeStream(resource.getResFile(), (size, is) -> is.readAllBytes());
 
