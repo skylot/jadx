b241613708353a0d03b3a81318b1a1d8fb586856
Mino260806 <mino260806@gmail.com>
2024-08-06 20:34:40 +0100

    remove unused statement

--- jadx-gui/src/main/java/jadx/gui/ui/tab/TabComponent.java
+++ jadx-gui/src/main/java/jadx/gui/ui/tab/TabComponent.java
@@ -67,7 +67,6 @@ public class TabComponent extends JPanel {
 		setOpaque(false);
 
 		JNode node = getNode();
-		TabBlueprint blueprint = getBlueprint();
 		label = new NodeLabel(buildTabTitle(node), node.disableHtml());
 		label.setFont(getLabelFont());
 		String toolTip = contentPanel.getTabTooltip();
