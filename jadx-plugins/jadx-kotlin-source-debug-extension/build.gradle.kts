d4f341934391e7d691c32083e9584ce9b25b0663
MrIkso <mrikso821@gmail.com>
2025-01-06 00:14:19 +0200

    feat: parse and use Kotlin SourceDebugExtension for rename classes and package

---
+++ jadx-plugins/jadx-kotlin-source-debug-extension/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+	id("jadx-library")
+	id("jadx-kotlin")
+}
+
+dependencies {
+	api(project(":jadx-core"))
+
+	testImplementation(project.project(":jadx-core").sourceSets.getByName("test").output)
+	testImplementation("org.apache.commons:commons-lang3:3.17.0")
+
+	testRuntimeOnly(project(":jadx-plugins:jadx-smali-input"))
+}
---
+++ jadx-plugins/jadx-kotlin-source-debug-extension/src/main/kotlin/jadx/plugins/kotlin/smap/model/Constants.kt
@@ -0,0 +1,5 @@
+package jadx.plugins.kotlin.smap.model
+
+object Constants {
+	const val KOTLIN_SOURCE_DEBUG_EXCEPTION = "Lkotlin/jvm/internal/SourceDebugExtension;"
+}
