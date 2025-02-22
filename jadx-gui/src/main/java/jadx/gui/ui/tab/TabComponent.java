cc7ff382a16b8306c7219da1f0b5dca85c09f069
glu0n <glu0n@gmail.com>
2025-02-22 14:17:56 +0200

    Adds separators between tab categories, similar to how tabs look in VSCode/IntelliJ

--- jadx-gui/src/main/java/jadx/gui/ui/tab/TabComponent.java
+++ jadx-gui/src/main/java/jadx/gui/ui/tab/TabComponent.java
@@ -230,11 +230,13 @@ public class TabComponent extends JPanel {
 			JMenuItem unbookmarkAll = new JMenuItem(NLS.str("tabs.unbookmark_all"));
 			unbookmarkAll.addActionListener(e -> tabsController.unbookmarkAllTabs());
 			menu.add(unbookmarkAll);
+			menu.addSeparator();
 		}
 		if (nodeFullName != null ) {
 			JMenuItem revealTab = new JMenuItem(NLS.str("tabs.reveal_in_explorer"));
 			revealTab.addActionListener(e -> tabsController.getMainWindow().selectNodeInTree(getNode()));
 			menu.add(revealTab);
+			menu.addSeparator();
 		}
 
 		JMenuItem closeTab = new JMenuItem(NLS.str("tabs.close"));
