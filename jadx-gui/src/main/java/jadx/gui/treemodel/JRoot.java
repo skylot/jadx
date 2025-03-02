18fbf5925bfe03afde014d0cdbcedd82854ec820
Skylot <118523+skylot@users.noreply.github.com>
2025-03-02 21:14:31 +0000

    fix(gui): allow file and directory have same name in tree

--- jadx-gui/src/main/java/jadx/gui/treemodel/JRoot.java
+++ jadx-gui/src/main/java/jadx/gui/treemodel/JRoot.java
@@ -64,19 +64,17 @@ public class JRoot extends JNode {
 			String[] parts = new File(rfName).getPath().split(splitPathStr);
 			JResource curRf = root;
 			int count = parts.length;
-			for (int i = 0; i < count; i++) {
+			for (int i = 0; i < count - 1; i++) {
 				String name = parts[i];
 				JResource subRF = getResourceByName(curRf, name);
 				if (subRF == null) {
-					if (i != count - 1) {
-						subRF = new JResource(null, name, JResType.DIR);
-					} else {
-						subRF = new JResource(rf, rf.getDeobfName(), name, JResType.FILE);
-					}
+					subRF = new JResource(null, name, JResType.DIR);
 					curRf.addSubNode(subRF);
 				}
 				curRf = subRF;
 			}
+			JResource leaf = new JResource(rf, rf.getDeobfName(), parts[count - 1], JResType.FILE);
+			curRf.addSubNode(leaf);
 		}
 		root.sortSubNodes();
 		root.update();
