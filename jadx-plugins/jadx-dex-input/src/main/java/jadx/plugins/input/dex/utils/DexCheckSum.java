01623de1990fdfcd89eeefbff9c6c0e49fe0fd19
Skylot <118523+skylot@users.noreply.github.com>
2025-12-02 18:51:03 +0000

    check if length can be read, use utils method to read int values

--- jadx-plugins/jadx-dex-input/src/main/java/jadx/plugins/input/dex/utils/DexCheckSum.java
+++ jadx-plugins/jadx-dex-input/src/main/java/jadx/plugins/input/dex/utils/DexCheckSum.java
@@ -1,20 +1,20 @@
 package jadx.plugins.input.dex.utils;
 
-import java.nio.ByteBuffer;
 import java.util.zip.Adler32;
 
 import jadx.plugins.input.dex.DexException;
 
-import static java.nio.ByteOrder.LITTLE_ENDIAN;
-
 public class DexCheckSum {
 
 	public static void verify(String fileName, byte[] content, int offset) {
-		int len = ByteBuffer.wrap(content, offset + 32, 4).order(LITTLE_ENDIAN).getInt();
+		if (offset + 32 + 4 > content.length) {
+			throw new DexException("Dex file truncated, can't read file length, file: " + fileName);
+		}
+		int len = DataReader.readU4(content, offset + 32);
 		if (offset + len > content.length) {
 			throw new DexException("Dex file truncated, length in header: " + len + ", file: " + fileName);
 		}
-		int checksum = ByteBuffer.wrap(content, offset + 8, 4).order(LITTLE_ENDIAN).getInt();
+		int checksum = DataReader.readU4(content, offset + 8);
 		Adler32 adler32 = new Adler32();
 		adler32.update(content, offset + 12, len - 12);
 		int fileChecksum = (int) adler32.getValue();
