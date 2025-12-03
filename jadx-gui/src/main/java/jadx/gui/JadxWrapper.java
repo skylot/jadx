74c52a88844604897fcf013957e5ef883e1b5cfe
Skylot <118523+skylot@users.noreply.github.com>
2025-12-03 19:38:27 +0000

    fix(gui): resolve exception in search dialog on project reload (#2714)

--- jadx-gui/src/main/java/jadx/gui/JadxWrapper.java
+++ jadx-gui/src/main/java/jadx/gui/JadxWrapper.java
@@ -86,11 +86,13 @@ public class JadxWrapper {
 
 	// TODO: check and move into core package
 	public void unloadClasses() {
-		for (ClassNode cls : getDecompiler().getRoot().getClasses()) {
-			ProcessState clsState = cls.getState();
-			cls.unload();
-			cls.setState(clsState == PROCESS_COMPLETE ? GENERATED_AND_UNLOADED : NOT_LOADED);
-		}
+		getCurrentDecompiler().ifPresent(decompiler -> {
+			for (ClassNode cls : decompiler.getRoot().getClasses()) {
+				ProcessState clsState = cls.getState();
+				cls.unload();
+				cls.setState(clsState == PROCESS_COMPLETE ? GENERATED_AND_UNLOADED : NOT_LOADED);
+			}
+		});
 	}
 
 	public void close() {
