57fd9b5bdb398d3fb6ebc2bf867b6c935ee08025
ewt45 <79033456+ewt45@users.noreply.github.com>
2026-03-30 03:07:04 +0800

    fix(gui): add file end line feed for exported code (PR #2836)
    chore: add file end line feed for export code

--- jadx-gui/src/main/java/jadx/gui/ui/popupmenu/JResourcePopupMenu.java
+++ jadx-gui/src/main/java/jadx/gui/ui/popupmenu/JResourcePopupMenu.java
@@ -1,8 +1,6 @@
 package jadx.gui.ui.popupmenu;
 
 import java.io.IOException;
-import java.io.Writer;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
@@ -16,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import jadx.api.plugins.utils.CommonFileUtils;
+import jadx.core.dex.visitors.SaveCode;
 import jadx.gui.treemodel.JResource;
 import jadx.gui.ui.MainWindow;
 import jadx.gui.ui.filedialog.FileDialogWrapper;
@@ -147,11 +146,7 @@ public class JResourcePopupMenu extends JPopupMenu {
 	}
 
 	private static void exportString(JResource resource, Path savePath) {
-		try (Writer writer = Files.newBufferedWriter(savePath, StandardCharsets.UTF_8)) {
-			writer.write(resource.getCodeInfo().getCodeStr());
-		} catch (Exception e) {
-			throw new RuntimeException("Error saving file " + resource.getName(), e);
-		}
+		SaveCode.save(resource.getCodeInfo().getCodeStr(), savePath.toFile());
 	}
 
 }
