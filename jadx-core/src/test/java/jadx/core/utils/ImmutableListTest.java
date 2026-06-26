fc92d2d72d259f9da4567186672eb360c2ea7a22
Skylot <118523+skylot@users.noreply.github.com>
2026-06-26 21:22:57 +0100

    fix code style

--- jadx-core/src/test/java/jadx/core/utils/ImmutableListTest.java
+++ jadx-core/src/test/java/jadx/core/utils/ImmutableListTest.java
@@ -1,8 +1,8 @@
 package jadx.core.utils;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ImmutableListTest {
 
