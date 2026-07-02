6b973d32cf161ebf1f10ebdf9c72eb4eba8f7399
amirdeljouyi <amirdeljouyi@gmail.com>
2026-07-03 00:26:55 +0200

    test: cover class generation scenarios

--- jadx-core/src/test/java/jadx/tests/integration/others/TestClassGen.java
+++ jadx-core/src/test/java/jadx/tests/integration/others/TestClassGen.java
@@ -19,6 +19,28 @@ public class TestClassGen extends IntegrationTest {
 		}
 	}
 
+	public static class GenericCls<T extends Number & Comparable<T>> {
+		private final T value;
+
+		public GenericCls(T value) {
+			this.value = value;
+		}
+
+		public <R extends Runnable> R get(R runnable) {
+			return runnable;
+		}
+	}
+
+	public static class MultiInterfaceCls implements Runnable, AutoCloseable {
+		@Override
+		public void run() {
+		}
+
+		@Override
+		public void close() {
+		}
+	}
+
 	@Test
 	public void test() {
 		JadxAssertions.assertThat(getClassNode(TestCls.class))
@@ -29,5 +51,24 @@ public class TestClassGen extends IntegrationTest {
 				.contains(indent(2) + "int test3();")
 				.contains("public static abstract class A {")
 				.contains(indent(2) + "public abstract int test2();");
+	}
+
+	@Test
+	public void testGenericBounds() {
+		JadxAssertions.assertThat(getClassNode(GenericCls.class))
+				.code()
+				.contains("public class TestClassGen$GenericCls<T extends Number & Comparable<T>> {")
+				.contains("private final T value;")
+				.contains("public TestClassGen$GenericCls(T value) {")
+				.contains("public <R extends Runnable> R get(R runnable) {");
+	}
+
+	@Test
+	public void testMultipleInterfaces() {
+		JadxAssertions.assertThat(getClassNode(MultiInterfaceCls.class))
+				.code()
+				.contains("public class TestClassGen$MultiInterfaceCls implements Runnable, AutoCloseable {")
+				.contains("public void run() {")
+				.contains("public void close() {");
 	}
 }
