dfedeba157fde70853ae7945741f509032933ebd
hd <hdshare@vip.qq.com>
2026-09-18 01:27:28 +0800

    fix(gui): handle unopened projects in reopen

--- jadx-gui/src/main/java/jadx/gui/settings/ui/JadxSettingsWindow.java
+++ jadx-gui/src/main/java/jadx/gui/settings/ui/JadxSettingsWindow.java
@@ -769,11 +769,7 @@ public class JadxSettingsWindow extends JDialog {
 		SwingUtilities.invokeLater(() -> {
 			if (shouldReload()) {
 				mainWindow.getShortcutsController().loadSettings();
-				if (mainWindow.getProject().getFilePaths().isEmpty()) {
-					mainWindow.reloadMenuShortcuts();
-				} else {
-					mainWindow.reopen();
-				}
+				mainWindow.reopen();
 			}
 			if (!settings.getLangLocale().equals(prevLang)) {
 				JOptionPane.showMessageDialog(
